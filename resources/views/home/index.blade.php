15236c021306b8b90cc6acdde8f5c0a22446ea6f
yi xuan <3a832061@gm.student.ncut.edu.tw>
2021-12-06 19:06:13 +0800

    練習2 子樣板修正

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,14 +1,7 @@
 <!DOCTYPE html>
-<html>
     @extends('layouts.master')
     @section('title','Home')
     @section('content')
-	<head>
-		<title>Home</title>
-	</head>
-	<body>
 		<h1>Home</h1>
         <p>This is my body content.</p>
-	</body>
     @endsection
-</html>
